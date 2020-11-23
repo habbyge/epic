2b29e67b888c7f495b664af1335dcb0e89c6f6d2
habbyge <habbyge@tencent.com>
2020-11-23 11:28:25 +0800

    ignore(iCrash): 分析epic

--- library/src/main/cpp/epic.cpp
+++ library/src/main/cpp/epic.cpp
@@ -1,4 +1,4 @@
-/*
+/**
  * Original Copyright 2014-2015 Marvin Wißfeld
  * Modified work Copyright (c) 2017, weishu
  *
@@ -16,30 +16,99 @@
  */
 
 /**
- * Android本身是裁减过的Linux，好些system call不能使用，另外由于没有采用glibc（用的是Bionic libc).
- * 好些函数所在的头文件位置也有变化，这都给移植工作带来困难。更为坑爹的是一些函数在头文件里能找到定义在
- * 具体库里确没有实现（比如：pthread_mutex_timedlock）. Android Native开发在编译链接阶段会遇到
- * 上述“惨痛”经历，但更为痛苦的是好不容易变成可执行文件，一运行就Crash没有任何信息。遇到这种情况，在
- * 排除了代码有低级错误的情况后，最终只能想办法做debug.
+ * Android本身是裁减过的Linux,好些system call不能使用,另外由于没有采用glibc(用的是Bionic libc)
+ * 好些函数所在的头文件位置也有变化，这都给移植工作带来困难。更为坑爹的是一些函数在头文件里能找到定义
+ * 在具体库里确没有实现（比如：pthread_mutex_timedlock）. Android Native开发在编译链接阶段会遇
+ * 到上述“惨痛”经历，但更为痛苦的是好不容易变成可执行文件，一运行就Crash没有任何信息。遇到这种情况，
+ * 在排除了代码有低级错误的情况后，最终只能想办法做debug.
  * 
  * http://weishu.me/2017/11/23/dexposed-on-art/
  * 
  * ART有什么特别的？
- * 为什么Dexposed能够在Dalvik上为所欲为，到ART时代就不行了呢？排除其他非技术因素来讲，ART确实比Dalvik复杂太多；
- * 更要命的是，从Android L到Android O，每一个Android版本中的ART变化都是天翻地覆的，大致列举一下：
- * Android L(5.0/5.1) 上的ART是在Dalvik上的JIT编译器魔改过来的，名为quick（虽然有个portable编译器，但是从未
- * 启用过）；这个编译器会做一定程度的方法内联，因此很多基于入口替换的Hook方式一上来就跪了。Android M(6.0) 上的
- * ART编译器完全重新实现了：Optimizing。且不说之前在Android L上的Hook实现要在M上重新做一遍，这个编译器的寄存器
- * 分配比quick好太多，结果就是hook实现的时候你要是乱在栈或者寄存器上放东西，代码很容易就跑飞。Android N(7.0/7.1) 
- * N 开始采用了混合编译的方式，既有AOT也有JIT，还伴随着解释执行；混合模式对Hook影响是巨大的，以至于Xposed直到今年
- * 才正式支持Android N。首先JIT的出现导致方法入口不固定，跑着跑着入口就变了，更麻烦的是还会有OSR（栈上替换），不仅
- * 入口变了，正在运行时方法的汇编代码都可能发生变化；其次，JIT的引入带来了更深度的运行时方法内联，这些都使得虚拟机层
- * 面的Hook更为复杂。
- * Android O(8.0) Android O的Runtime做了很多优化，传统Java VM有的一些优化手段都已经实现，比如类层次分析，循环
- * 优化，向量化等；除此之外，DexCache被删除，跨dex方法内联以及Concurrent compacting GC的引入，使得Hook技术变
- * 的扑朔迷离。
- * 可以看出，ART不仅复杂，而且还爱折腾；一言不合就魔改，甚至重写。再加上Android的碎片化，这使得实现一个稳定的虚拟机
- * 层面上运行时Java Method AOP几无可能。
+ * 为什么Dexposed能够在Dalvik上为所欲为，到ART时代就不行了呢？排除其他非技术因素来讲，ART确实比
+ * Dalvik复杂太多；更要命的是，从Android L到Android O，每一个Android版本中的ART变化都是天翻地
+ * 覆的，大致列举一下：Android L(5.0/5.1) 上的ART是在Dalvik上的JIT编译器魔改过来的，名为quick
+ * （虽然有个portable编译器，但是从未启用过）；这个编译器会做一定程度的方法内联，因此很多基于入口替
+ * 换的Hook方式一上来就跪了。Android M(6.0) 上的ART编译器完全重新实现了：Optimizing。且不说之前
+ * 在Android L上的Hook实现要在M上重新做一遍，这个编译器的寄存器分配比quick好太多，结果就是hook实
+ * 现的时候你要是乱在栈或者寄存器上放东西，代码很容易就跑飞。Android N(7.0/7.1) N 开始采用了混合
+ * 编译的方式，既有AOT也有JIT，还伴随着解释执行；混合模式对Hook影响是巨大的，以至于Xposed 直到今年
+ * 才正式支持Android N。首先JIT的出现导致方法入口不固定，跑着跑着入口就变了，更麻烦的是还会有OSR
+ * （栈上替换），不仅入口变了，正在运行时方法的汇编代码都可能发生变化；其次，JIT的引入带来了更深度的
+ * 运行时方法内联，这些都使得虚拟机层面的Hook更为复杂。Android O(8.0) Android O的Runtime做了很
+ * 多优化，传统Java VM有的一些优化手段都已经实现，比如类层次分析，循环优化，向量化等；除此之外，
+ * DexCache被删除，跨dex方法内联以及Concurrent compacting GC 的引入，使得Hook技术变的扑朔迷离.
+ * 可以看出，ART不仅复杂，而且还爱折腾；一言不合就魔改，甚至重写。再加上Android的碎片化，这使得实现
+ * 一个稳定的虚拟机层面上运行时Java Method AOP几无可能。
+ * 
+ * 在ART中，每一个Java方法在虚拟机内部都由一个ArtMethod对象表示（Native层，实际上是一个C++对象）,
+ * 这个native 的 ArtMethod对象包含了此Java方法的所有信息，比如名字，参数类型，方法本身代码的入口
+ * 地址（entrypoint)等；暂时放下trampoline以及interpreter和jit不谈，一个Java方法的执行非常简单:
+ * 1. 想办法拿到这个Java方法所代表的ArtMethod对象
+ * 2. 取出其entrypoint，然后跳转到此处开始执行(所有entrypoint均指compiled_code_entry_point)
+ * 
+ * 从以上两点来看，一种很自然的Hook方案是: "直接替换entrypoint"，通过把原方法对应的ArtMethod对象
+ * 的entrypoint替换为目标方法的entrypoint，可以使得原方法被调用过程中取entrypoint的时候拿到的是
+ * 目标方法的entry，进而直接跳转到目标方法的.text段；从而达到Hook的目的。
+ * 这也是 AndFix 的热补丁方案，Sophix 对这个方案做了一些改进，也即整体替换，不过原理上都一样。二者
+ * 在替换方法之后把原方法直接丢弃，因此无法实现AOP。AndroidMethodHook 基于Sophix的原理，用
+ * dexmaker动态生成类，将原方法保存下来，从而实现了AOP。
+ * 
+ * Epic基本原理:
+ * 一个较为通用的Hook技术，几乎只有一条路———基于dynamic dispatch的
+ * dynamic callee-side rewriting.
+ * 
+ * ARM汇编特点1: https://cloud.tencent.com/developer/article/1658622
+ * 
+ * 特点1：ARM采用RISC架构
+ * CPU本身不能直接读取内存，而需要先将内存中内容加载入CPU中通用寄存器中才能被CPU
+ * 处理：ldr（load register）将内存中的内容加载到通用寄存器中，str(store register)将进村器中的
+ * 内容存储到内存空间中，ldr/str组合用来实现 ARM CPU和内存数据交换.
+ * 
+ * ARM汇编特点2：8种寻址方式。
+ * 寄存器寻址    mov r1, r2 把r2寄存器的值加载到r1当中。
+ * 立即寻址      mov r0, #0xFF00  将立即数的值赋给r0
+ * 寄存器移位寻址 mov r0, r1, lsl #3 
+ * 寄存器间接寻址 ldr r1, [r2]
+ * 基址变址寻址   ldr r1, [r2, #4]
+ * 多寄存器寻址  ldmia r1!, {r2-r7,r12}
+ * 堆栈寻址      stmfd sp!, {r2-r7,lr}
+ * 相对寻址      beqflag
+ *
+ * ARM汇编特点3：指令后缀。同一指令经常附带不同后缀，变成不同的指令。经常使用的后缀有：
+ * B（byte）功能不变，操作长度变为8位
+ * H（half word）功能不变，长度变为16位
+ * S（signed）功能不变，操作数变为有符号，如 ldr ldrb ldrh ldrsb ldrsh
+ * S（S标志）功能不变，影响CPSR标志位，如 mov和movs  movs r0, #0
+ * 
+ * ARM汇编特点4：多级指令流水线。为增加处理器指令流的速度，ARM使用多级流水线。所以要注意PC指向正被
+ * 取指的指令，而非正在执行的指令。
+ * 
+ * cmp r0, r1  比较r0和r1寄存器值是否相等，若相等，会改变cpsr寄存器对应的零标志位
+ * 跳转指令b、bl、bx
+ * 
+ * Arm汇编中的一些规则：https://www.jianshu.com/p/0277280d8e35
+ * 栈与栈帧
+ * 默认满栈降序规则，即PUSH先减后放，POP先取后加。
+ * 栈帧寄存器指向函数前序期压栈保存寄存器前的栈底。一般先压LR后R11(FP),之后才申请栈上空间(即减SP)。
+ * 结束时直接依据FP恢复LR以及上层FP。
+ * 
+ * 有必要说明一下ART中的函数的调用约定。以Thumb2为例，子函数调用的参数传递是通过寄存器r0~r3 以及sp
+ * 寄存器完成的。r0 ~ r3 依次传递第一个至第4个参数，同时 sp, (sp + 4), (sp + 8), (sp + 12) 
+ * 也存放着r0~r3上对应的值；多余的参数通过 sp传递，比如 *(sp + 16)放第四个参数，以此类推。同时，函
+ * 数的返回值放在r0寄存器。如果一个参数不能在一个寄存器中放下，那么会占用2个或多个寄存器.
+ * 
+ * 在ART中，r0寄存器固定存放被调用方法的ArtMethod指针，如果是non-static 方法，r1寄存器存放方法的
+ * this对象；另外，只有long/double 占用8bytes，其余所有基本类型和对象类型都占用4bytes。不过这只
+ * 是基本情形，不同的ART版本对这个调用约定有不同的处理，甚至不完全遵循。
+ * 
+ * Arm的3个特殊寄存器: sp(r13)/lr(r14)/pc(r15)
+ * sp：帧指针寄存器
+ * lr: 连接寄存器 (1) 保存子程序返回地址；(2) 当异常发生时，异常模式的r14用来保存异常返回地址，将
+ *     r14如栈可以处理嵌套中断
+ * 程序计数器r15(pc)
+ * 
+ * trampoline2 蹦床函数
  */
 
 #include <jni.h>
@@ -56,7 +125,8 @@
 
 #undef NDEBUG
 #ifdef NDEBUG
-#define LOGV(...)  ((void)__android_log_print(ANDROID_LOG_INFO, "epic.Native", __VA_ARGS__))
+#define LOGV(...)  ((void)__android_log_print(ANDROID_LOG_INFO, \
+        "epic.Native", __VA_ARGS__))
 #else
 #define LOGV(...)
 #endif
@@ -66,11 +136,12 @@
 jobject (*addWeakGloablReference)(JavaVM*, void*, void*) = nullptr;
 
 void* (*jit_load_)(bool*) = nullptr;
-void* jit_compiler_handle_ = nullptr;
+void* jit_compiler_handle_= nullptr;
 bool (*jit_compile_method_)(void*, void*, void*, bool) = nullptr;
 
 typedef bool (*JIT_COMPILE_METHOD1)(void*, void*, void*, bool);
-typedef bool (*JIT_COMPILE_METHOD2)(void*, void*, void*, bool, bool); // Android Q
+// Android Q
+typedef bool (*JIT_COMPILE_METHOD2)(void*, void*, void*, bool, bool); 
 
 void (*jit_unload_)(void*) = nullptr;
 
@@ -109,7 +180,8 @@ static int api_level;
  */
 void init_entries(JNIEnv* env) {
 
-    // 获取Android系统 SDK 版本：adb shell getprop ro.build.version.release 我的机器上返回: 10
+    // 获取Android系统 SDK 版本：adb shell getprop ro.build.version.release 
+    // 我的机器上返回: 10.
     // 获取Android系统 API 版本：adb shell getprop ro.build.version.sdk 我的机器上返回：29
     char api_level_str[5];
     __system_property_get("ro.build.version.sdk", api_level_str);
@@ -118,10 +190,10 @@ void init_entries(JNIEnv* env) {
     LOGV("api level: %d", api_level);
     
     if (api_level < 23) {
-        // RTLD_LAZY: 在dlopen()返回前，对于动态库中存在的未定义的变量(如外部变量extern，也可以是函数)不
-        // 执行解析，就是不解析这个变量的地址。
-        // RTLD_NOW：与上面不同，他需要在dlopen返回前，解析出每个未定义变量的地址，如果解析不出来，在dlopen
-        // 会返回NULL，错误为: undefined symbol: xxxx.......
+        // RTLD_LAZY: 在dlopen()返回前，对于动态库中存在的未定义的变量(如外部变量extern，也可
+        //  以是函数)不执行解析，就是不解析这个变量的地址。
+        // RTLD_NOW：与上面不同，他需要在dlopen返回前，解析出每个未定义变量的地址，如果解析不出
+        // 来，在dlopen会返回NULL，错误为: undefined symbol: xxxx.......
         // RTLD_GLOBAL: 它的含义是使得so库中的解析的定义变量在随后的其它的链接库中变得可以使用。
 
         // Android L, art::JavaVMExt::AddWeakGlobalReference(art::Thread*, art::mirror::Object*)
@@ -237,7 +309,6 @@ jboolean epic_munprotect(JNIEnv* env, jclass, jlong addr, jlong len) {
     return JNI_TRUE;
 }
 
-// TODO: ing......
 jboolean epic_cacheflush(JNIEnv* env, jclass, jlong addr, jlong len) {
 #if defined(__arm__)
     int i = cacheflush(addr, addr + len, 0);
@@ -270,11 +341,10 @@ void epic_memput(JNIEnv* env, jclass, jbyteArray src, jlong dest) {
         // LOGV("put %d with %d", i, *(srcPnt + i));
         destPnt[i] = (unsigned char) srcPnt[i];
     }
-    env->ReleaseByteArrayElements(src, srcPnt, 0);
+    env->ReleaseByteArrayElements(src, srcPnt, 0); // TODO: ing......
 }
 
 jbyteArray epic_memget(JNIEnv* env, jclass, jlong src, jint length) {
-
     jbyteArray dest = env->NewByteArray(length);
     if (dest == NULL) {
         return NULL;
@@ -290,8 +360,12 @@ jbyteArray epic_memget(JNIEnv* env, jclass, jlong src, jint length) {
 }
 
 jlong epic_mmap(JNIEnv* env, jclass, jint length) {
-    void* space = mmap(0, (size_t) length, PROT_READ | PROT_WRITE | PROT_EXEC,
-                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    
+    void* space = mmap(0, (size_t) length, 
+                       PROT_READ | PROT_WRITE | PROT_EXEC,
+                       MAP_PRIVATE | MAP_ANONYMOUS, 
+                       -1, 0);
+
     if (space == MAP_FAILED) {
         LOGV("mmap failed: %d", errno);
         return 0;
@@ -318,6 +392,7 @@ jobject epic_getobject(JNIEnv* env, jclass clazz, jlong self, jlong address) {
     JavaVM* vm;
     env->GetJavaVM(&vm);
     LOGV("java vm: %p, self: %p, address: %p", vm, (void*) self, (void*) address);
+
     jobject object = addWeakGloablReference(vm, (void*) self, (void*) address);
     return object;
 }
@@ -328,15 +403,17 @@ jlong epic_getMethodAddress(JNIEnv* env, jclass clazz, jobject method) {
 }
 
 jboolean epic_isGetObjectAvaliable(JNIEnv*, jclass) {
-    return (jboolean)(addWeakGloablReference != nullptr);
+    return (jboolean) (addWeakGloablReference != nullptr);
 }
 
 jlong pc, jlong sizeOfDirectJump,
+
 jboolean epic_activate(JNIEnv* env, jclass jclazz, jlong jumpToAddress, 
                        jlong sizeOfBridgeJump, jbyteArray code) {
 
     // fetch the array, we can not call this when thread suspend(may lead deadlock)
-    jbyte* srcPnt = env->GetByteArrayElements(code, 0);
+    // 获取Java层的字节数组：code，转换为JNI能用的字节数组 和 长度
+    jbyte* srcPnt = env->GetByteArrayElements(code, JNI_FALSE);
     jsize length = env->GetArrayLength(code);
 
     jlong cookie = 0;
@@ -346,8 +423,8 @@ jboolean epic_activate(JNIEnv* env, jclass jclazz, jlong jumpToAddress,
         // 1. modify the code mprotect
         // 2. modify the code
 
-        // Ideal, this two operation must be atomic. Below N, this is safe, because no one
-        // modify the code except ourselves;
+        // Ideal, this two operation must be atomic. Below N, this is safe, 
+        // because no one modify the code except ourselves;
         // But in Android N, When the jit is working, between our step 1 and step 2,
         // if we modity the mprotect of the code, and planning to write the code,
         // the jit thread may modify the mprotect of the code meanwhile
@@ -408,18 +485,13 @@ static int registerNativeMethods(JNIEnv* env, const char* className,
     if (clazz == NULL) {
         return JNI_FALSE;
     }
-
     if (env->RegisterNatives(clazz, gMethods, numMethods) < 0) {
         return JNI_FALSE;
     }
-
     return JNI_TRUE;
 }
 
-JNIEXPORT jint
-
-JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
-
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
     JNIEnv* env = NULL;
 
     LOGV("JNI_OnLoad");
@@ -429,10 +501,12 @@ JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
     }
 
     if (!registerNativeMethods(env, JNIHOOK_CLASS, dexposedMethods,
-                               sizeof(dexposedMethods) / sizeof(dexposedMethods[0]))) {
+            sizeof(dexposedMethods) / sizeof(dexposedMethods[0]))) {
+                
         return -1;
     }
 
     init_entries(env);
+
     return JNI_VERSION_1_6;
 }
