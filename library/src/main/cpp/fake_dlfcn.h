4c4c6df06b5a0c5aa097fcc8c6f6cec3e3082af4
habbyge <habbyge@tencent.com>
2021-01-26 15:09:39 +0800

    xxx

--- library/src/main/cpp/fake_dlfcn.h
+++ library/src/main/cpp/fake_dlfcn.h
@@ -21,7 +21,12 @@
 /**
  * Java中的System.loadLibrary(libName)，会调用到 binoc/linker.cc 中的 linker.cc 的 do_dlopen()，
  * 本质上是 #include <fcntl.h> 中的 dlopen()等系列函数，这里实际上给我的启示是 读取so文件可以直接使用
- * fcntl.h 中的函数.
+ * fcntl.h 中的函数(android_namespace_t是在Android7.0及以后引入的命名空间机制，它把应用层的共享库与系
+ * 统层的共享库区分开来，并限制对部分系统方法的访问)，do_dlopen()会调用find_library()来查找和加载so，并
+ * 返回 soinfo结构体，本质是调用 <fcntl.h> 中的 dlopen(const char* soPathName, int mode)，返回void*，
+ * 就是指向 struct soinfo的指针，soinfo->call_constructors() 表示在手动显式的加载so时，加载完成后系
+ * 统会调用其指定的构造函数，这个调用会比JNI_OnLoad更早，其指定方式是在函数前加上属性:
+ * __attribute__((constructor))。
  */
 
 #ifndef DEXPOSED_DLFCN_H
